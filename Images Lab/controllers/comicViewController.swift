e6136f76b9b726cff83f0e635386820583509265
uwagboe <sm.uwagboe@gmail.com>
2019-12-16 17:43:23 -0500

    set up the comic view controller and added the respective elements and connected them.

--- Images Lab/controllers/comicViewController.swift
+++ Images Lab/controllers/comicViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class comicViewController: UIViewController {
 
+    // outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var textField: UITextField!
+    
+//    var comic: Comic?{
+//        didSet{
+//            imageView.image = ..
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    func updateUI() {
+        // default comic image
+        // set up stepper value
+        //
+    }
+    
+    
+    
+    @IBAction func mostRecentButton(_ sender: UIButton) {
+         //should go to the most recent comic.
+        // sets the stepper value and the text field..
+        
+    }
+    
+    
+    @IBAction func randomButton(_ sender: UIButton) {
+        //should go to a random comic
+        // sets the stepper value and the text field..
 
-
+        
+    }
+    
+    
 }
 
