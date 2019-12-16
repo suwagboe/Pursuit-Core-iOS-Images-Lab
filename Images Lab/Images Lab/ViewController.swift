6366ee87f007bfd6402f7db3a411eb8f70d8809f
uwagboe <sm.uwagboe@gmail.com>
2019-12-16 17:17:38 -0500

    practice push

---
+++ Images Lab/Images Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Images Lab
+//
+//  Created by Pursuit on 12/16/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Images Lab/Images LabTests/Images_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  Images_LabTests.swift
+//  Images LabTests
+//
+//  Created by Pursuit on 12/16/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import XCTest
+@testable import Images_Lab
+
+class Images_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
